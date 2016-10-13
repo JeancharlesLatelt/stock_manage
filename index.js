5f66f64d70ff6e85de759a91b88046182ac0189e
meffre_q <quentin.meffre@epitech.eu>
2016-10-13 16:42:06 +0200

    fix lib

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-let Hapi = require('hapi');
-let server = Hapi.createServer('0.0.0.0', 8080);
+'use strict';
 
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: function(req, reply) {
-    reply('test');
-  }
+const Hapi = require('hapi');
+const mongojs = require('mongojs');
+const server = new Hapi.Server();
+
+server.connection({
+    port: 8080
 });
-server.start();
+server.app.db = mongojs('stock_manage', ['user']);
+server.register([
+    require('./routes/login.js')
+], (err) => {
+    if (err) {
+        throw err;
+    }
+    server.start((err) => {
+        console.log('Server running at:', server.info.uri);
+    });
+});
